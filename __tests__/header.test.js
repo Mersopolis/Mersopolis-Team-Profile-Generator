fc16698b539b33f343f2ea70df7e85c02db26108
Mersopolis <mersopolis@gmail.com>
2023-04-01 10:15:18 -0600

    HTML Generates. Fixing Display.

--- __tests__/header.test.js
+++ __tests__/header.test.js
@@ -1,11 +1,10 @@
 const Header = require('../lib/header.js');
-const {  } = require('../lib/');
 
 describe('Header', () => {
   test('should render header with the date', () => {
     const expectedHtml = [
       '<header class="header">',
-      `<h1>${title}'s Team</h1>`,
+      `<h1>${title}</h1>`,
       '</header>',
     ].join('');
     const header = new Header();
